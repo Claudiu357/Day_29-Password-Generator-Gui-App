77aed58ff1fac7b189ae003a794ec240000622f3
Claudiu357 <neculaclaudiu21@gmail.com>
2025-02-18 18:22:05 +0100

    now the add button is working and it s displayed a message box with the detailes saved

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from  tkinter import *
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = web_input.get()
+    email_username = email_username_input.get()
+    password = password_input.get()
+
+    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\nEmail: {email_username}"
+                           f"\nPassword:{password}\nIs it ok to save?")
+    if is_ok:
+        with open("pass.txt", "a") as file:
+            file.write(website + " | " + email_username + " | " + password + "\n")
+            web_input.delete(0, END)
+            password_input.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -18,12 +31,14 @@ website_label.grid(row=1, column=0)
 
 web_input = Entry(width=52)
 web_input.grid(row=1, column= 1, columnspan = 2)
+web_input.focus()
 
 email_username_label = Label(text="Email/Username:")
 email_username_label.grid(row=2, column = 0)
 
 email_username_input = Entry(width=52)
 email_username_input.grid(row = 2, column= 1, columnspan = 2)
+email_username_input.insert(0, "neculaclaudiu21@gmail.com")
 
 password_label = Label(text = "Password:  ")
 password_label.grid(row=3,column=0)
@@ -34,7 +49,7 @@ password_input.grid(row = 3, column = 1)
 generate_pass_button = Button(text="Generate Password")
 generate_pass_button.grid(row=3, column= 2)
 
-add_button = Button(text="Add", width=44)
+add_button = Button(text="Add", width=44, command=save_password)
 add_button.grid(row=4,column=1,columnspan=2)
 
 
